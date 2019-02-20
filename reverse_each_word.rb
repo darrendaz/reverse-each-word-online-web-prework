289dfb682cf1067a14ff67159b1f3d0e393d4426
Darren L <darren.lou@gmail.com>
2019-02-20 09:56:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  sentence = sentence.split(" ")
-  sentence.each { |word|
+  sentence.split.each { |word|
     return word.reverse
   }
   return sentence.join(" ")
